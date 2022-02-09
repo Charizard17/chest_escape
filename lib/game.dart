d5eeb4d67ff61fecae7912a9a8a14ae21b365cab
Esad Dursun <3723esad@gmail.com>
2022-02-09 23:35:40 +0100

    spritesheet added. sprite animation component created

--- lib/game.dart
+++ lib/game.dart
@@ -1,5 +1,25 @@
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
 
 class WallPasserGame extends FlameGame {
+  final SpriteAnimationComponent darkSoldier = SpriteAnimationComponent(
+    size: Vector2(130, 130),
+    position: Vector2(130, 300),
+  );
 
+  @override
+  Future<void>? onLoad() async {
+    final spriteSheet = SpriteSheet(
+      image: await Flame.images.load('darksoldier_spritesheet.png'),
+      srcSize: Vector2(64, 64),
+    );
+
+    darkSoldier.animation = spriteSheet.createAnimation(row: 1, stepTime: 0.1);
+
+    add(darkSoldier);
+
+    return super.onLoad();
+  }
 }
