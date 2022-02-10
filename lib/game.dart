4a011490f7a93134058ad94e02072b6ac3114c3f
Esad Dursun <3723esad@gmail.com>
2022-02-10 22:51:22 +0100

    joystick component added

--- lib/game.dart
+++ lib/game.dart
@@ -1,21 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:flame/sprite.dart';
+import 'package:flame/input.dart';
+import 'package:flame/palette.dart';
 
 import './player.dart';
 
-class WallPasserGame extends FlameGame {
+class WallPasserGame extends FlameGame with HasTappables, HasDraggables {
   late Player _player;
 
   @override
   Future<void>? onLoad() async {
+    final joystick = JoystickComponent(
+      knob: CircleComponent(
+        radius: 24,
+        paint: BasicPalette.white.withAlpha(100).paint(),
+      ),
+      background: CircleComponent(
+        radius: 60,
+        paint: BasicPalette.white.withAlpha(100).paint(),
+      ),
+      margin: const EdgeInsets.only(right: 50, bottom: 40),
+    );
+    add(joystick);
+
     _player = Player(
       position: Vector2(150, 150),
       size: Vector2(100, 100),
       anchor: Anchor.center,
+      joystick: joystick,
     );
-
     add(_player);
 
     return super.onLoad();
--- lib/player.dart
+++ lib/player.dart
@@ -3,14 +3,19 @@ import 'package:flame/components.dart';
 import 'package:flame/sprite.dart';
 
 class Player extends SpriteAnimationComponent {
+  final JoystickComponent joystick;
+  final double _speed = 80;
+
   late final SpriteAnimation _upWalkAnimation;
+  late final SpriteAnimation _downWalkAnimation;
   late final SpriteAnimation _leftWalkAnimation;
   late final SpriteAnimation _rightWalkAnimation;
 
   Player({
-    Vector2? position,
-    Vector2? size,
-    Anchor? anchor,
+    required Vector2? position,
+    required Vector2? size,
+    required Anchor? anchor,
+    required this.joystick,
   }) : super(position: position, size: size, anchor: anchor);
 
   @override
@@ -22,6 +27,8 @@ class Player extends SpriteAnimationComponent {
 
     _upWalkAnimation =
         spriteSheet.createAnimation(row: 1, stepTime: 0.11, from: 0, to: 5);
+    _downWalkAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: 0.11, from: 0, to: 5);
     _leftWalkAnimation =
         spriteSheet.createAnimation(row: 3, stepTime: 0.11, from: 0, to: 4);
     _rightWalkAnimation =
@@ -30,5 +37,28 @@ class Player extends SpriteAnimationComponent {
     this.animation = _upWalkAnimation;
 
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (!joystick.delta.isZero()) {
+      position.add(joystick.relativeDelta * _speed * dt);
+    }
+
+    if (joystick.direction == JoystickDirection.left ||
+        joystick.direction == JoystickDirection.downLeft ||
+        joystick.direction == JoystickDirection.upLeft) {
+      this.animation = _leftWalkAnimation;
+    } else if (joystick.direction == JoystickDirection.right ||
+        joystick.direction == JoystickDirection.downRight ||
+        joystick.direction == JoystickDirection.upRight) {
+      this.animation = _rightWalkAnimation;
+    } else if (joystick.direction == JoystickDirection.down) {
+      this.animation = _downWalkAnimation;
+    } else {
+      this.animation = _upWalkAnimation;
+    }
+
+    super.update(dt);
   }
 }
